9097d9fc96bfc0d12354f9367d2fa98f89c7d517
nickweber <nweber2001@icloud.com>
2023-02-17 10:16:59 -0500

    implemented button text swap

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
 var button = document.getElementsByClassName("text")[0];
 var btnCurrentSelected = false;
-var firstPlayer;
-var secondPlayer;
+var firstPlayer, firstIndex;
+var secondPlayer, secondIndex;
 
+// var nameArr = ["Ke'Bryan Hayes", "Willie Stargell", "Oneil Cruz", "Rich Gossage", "Jason Kendall", 
+// 				"Bill Mazeroski", "Dave Parker", "Bryan Reynolds", "Honus Wagner"];
 
-function changeName() {
+var nodes = document.getElementsByClassName('text');
+
+for (var i = 0; i < nodes.length; i++) {
+   nodes[i].addEventListener('click', function(i) {
+      console.log('You clicked element #' + i);
 	if (btnCurrentSelected) {
 		//set second player to second button clicked
+		secondPlayer = nodes[i].textContent;
+		secondIndex = i;
 		if(firstPlayer === secondPlayer){
 			//if same button clicked twice
 			//reset button vars
+
 		} else {
 			//swap name content
+			nodes[firstIndex].textContent = secondPlayer;
+			nodes[secondIndex].textContent = firstPlayer;
 		}
 		btnCurrentSelected = false;
 	} else {
 		//set firstPlayer = to index of first selected
-
+		firstPlayer = nodes[i].textContent;
+		firstIndex = i;
 		btnCurrentSelected = true;
 	}
-	console.log(button.innerText);
-	button.textContent = "Willie Stargell";
+   }.bind(null, i));
 }
-
-button.addEventListener("click", changeName);
-
-//set variable equal to button click, as well as boolean
-//if no button has been clicked previously var equals that button
-//if prev button has been clicked then new button and old button switch names
-//if same button clicked twice boolean resets
